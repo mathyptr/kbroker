0c68325d946096dd0d843cd582460e8d51401951
Mathilde <50731814+mathyptr@users.noreply.github.com>
2025-08-04 16:22:23 +0200

    Add create_topic script and update Producer script

---
+++ k6/producer_test_bytes.js
@@ -0,0 +1,57 @@
+import { check } from "k6";
+// import kafka extension
+import {
+  Writer,
+  Connection,
+  SchemaRegistry,
+  SCHEMA_TYPE_BYTES,
+} from "k6/x/kafka"; 
+
+const brokers = ["kafka:9092"];
+const topic = "swam-qesm_topic_test_byte";
+
+const writer = new Writer({
+  brokers: brokers,
+  topic: topic,
+  autoCreateTopic: true,
+});
+
+const connection = new Connection({
+  address: brokers[0],
+});
+const schemaRegistry = new SchemaRegistry();
+
+if (__VU == 0) {
+  connection.createTopic({ topic: topic });
+}
+
+const payload = "test swam-qesm SCHEMA_TYPE_BYTES payload";
+
+export default function () {
+  for (let index = 0; index < 100; index++) {
+    let messages = [
+      {
+        key: schemaRegistry.serialize({
+          data: Array.from("swam-qesm-" + index, (x) => x.charCodeAt(0)),
+          schemaType: SCHEMA_TYPE_BYTES,
+        }),
+        value: schemaRegistry.serialize({
+          data: Array.from(payload, (x) => x.charCodeAt(0)),
+          schemaType: SCHEMA_TYPE_BYTES,
+        }),
+      },
+    ];
+
+    writer.produce({
+      messages: messages,
+    });
+  }
+}
+
+export function teardown(data) {
+  if (__VU == 0) {
+//    connection.deleteTopic(topic);
+  }
+  writer.close();
+  connection.close();
+}
---
+++ k6/producer_test_string.js
@@ -0,0 +1,66 @@
+import { check } from "k6";
+// import kafka extension
+import {
+  Writer,
+  Connection,
+  SchemaRegistry,
+  SCHEMA_TYPE_STRING,
+} from "k6/x/kafka"
+
+
+const brokers = ["kafka:9092"];
+const topic = "swam-qesm_topic";
+
+const writer = new Writer({
+  brokers: brokers,
+  topic: topic,
+  autoCreateTopic: true,
+});
+
+const connection = new Connection({
+  address: brokers[0],
+});
+const schemaRegistry = new SchemaRegistry();
+
+if (__VU == 0) {
+  connection.createTopic({ topic: topic });
+}
+
+export const options = {
+  thresholds: {
+    kafka_writer_error_count: ["count == 0"],
+  },
+};
+
+export default function () {
+  for (let index = 0; index < 100; index++) {
+    let messages = [
+      {
+        key: schemaRegistry.serialize({
+          data: "swam-qesm-test-string-key",
+          schemaType: SCHEMA_TYPE_STRING,
+        }),
+        value: schemaRegistry.serialize({
+          data: "swam-qesm-test-string-value",
+          schemaType: SCHEMA_TYPE_STRING,
+        }),
+        headers: {
+          mykey: "swam-qesmvalue",
+        },
+        offset: index,
+        partition: 0,
+        time: new Date(), // timestamp
+      },  
+    ];
+
+    writer.produce({ messages: messages });
+  }
+}
+
+export function teardown(data) {
+  if (__VU == 0) {
+//    connection.deleteTopic(topic);
+  }
+  writer.close();
+  connection.close();
+}
