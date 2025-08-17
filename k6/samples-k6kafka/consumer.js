7882d7fd71548088dc7d77d8e4fdba3142b232a3
Mathilde <50731814+mathyptr@users.noreply.github.com>
2025-08-17 10:02:43 +0200

    Add consumer script

---
+++ k6/samples-k6kafka/consumer.js
@@ -0,0 +1,71 @@
+import { check } from "k6";
+// import kafka extension
+import {
+  Reader,
+  Connection,
+  SchemaRegistry,
+  SCHEMA_TYPE_STRING,
+} from "k6/x/kafka"; // import kafka extension
+
+// Prints module-level constants
+// console.log(kafka);
+
+// load test config, used to populate exported options object:
+const config = JSON.parse(open('./config/config.json'));
+const brokers = config.brokers;
+const topic = config.topic_string;
+
+const reader = new Reader({
+  brokers: brokers,
+  topic: topic,
+});
+const connection = new Connection({
+  address: brokers[0],
+});
+const schemaRegistry = new SchemaRegistry();
+
+export const options = {
+  thresholds: {
+    // Base thresholds to see reader is working
+    kafka_reader_error_count: ["count == 0"],
+  },
+};
+
+export default function () {
+  // Read 10 messages only
+  let messages = reader.consume({ limit: 10 });
+
+  check(messages, {
+    "10 messages are received": (messages) => messages.length == 10,
+  });
+
+  check(messages[0], {
+    "Topic equals to swam-qesm_topic": (msg) => msg["topic"] == topic,
+    "Key is a string and is correct": (msg) =>
+      schemaRegistry.deserialize({
+        data: msg.key,
+        schemaType: SCHEMA_TYPE_STRING,
+      }) == "swam-qesm-test-string-key",
+    "Value is a string and is correct": (msg) =>
+      typeof schemaRegistry.deserialize({
+        data: msg.value,
+        schemaType: SCHEMA_TYPE_STRING,
+      }) == "string" &&
+      schemaRegistry.deserialize({
+        data: msg.value,
+        schemaType: SCHEMA_TYPE_STRING,
+      }) == "swam-qesm-test-string-value",
+    "Header equals {'mykey': 'swam-qesmvalue'}": (msg) =>
+      "mykey" in msg.headers &&
+      String.fromCharCode(...msg.headers["mykey"]) == "swam-qesmvalue",
+    "Time is past": (msg) => new Date(msg["time"]) < new Date(),
+    "Partition is zero": (msg) => msg["partition"] == 0,
+    "Offset is gte zero": (msg) => msg["offset"] >= 0,
+    "High watermark is gte zero": (msg) => msg["highWaterMark"] >= 0,
+  });
+}
+
+export function teardown(data) {
+  reader.close();
+  connection.close();
+}
